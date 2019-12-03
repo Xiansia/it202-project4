60d30971fe0e08cd4404ceff93207ff847f7c496
Conway Dang <cdang4@uic.edu>
2019-12-03 20:55:21 +0000

    render Square

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ class Board extends React.Component {
     };
   }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 }
 class Square extends React.Component {
